6ab2377337dddc7f20cae99c718c98dccbab64a7
Zach Barge <zachb.barge@gmail.com>
2023-11-15 12:12:52 -0500

    Added a function to get access token to use for endpoints.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 app.get('/api/v1/search', (req, res) => {
 	res.send({message: 'Song to search for'});
 });
+
+function getToken = async () => {
+	const {data} = await axios.post("https://accounts.spotify.com/api/token",
+		`grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`,
+		{
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			}
+		});
+
+	return data["access_token"];
+}
 
 app.listen(8080 , () => {
 	console.log('Listening on port 8080');
